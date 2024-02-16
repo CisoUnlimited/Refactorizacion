5d16007e32194ae974bb30f6c0fed2e6ed10b091
Ciso <manuel.zambudio.herrerias@gmail.com>
2024-02-16 10:17:01 +0100

    v0.1

---
+++ src/C/Window.java
@@ -0,0 +1,18 @@
+package C;
+
+public class Window {
+    public Window(int width, int height){
+        //...
+    }
+    public void setSize(int width, int height){
+        //...
+    }
+    public boolean overlaps(Window w){
+        //...
+        //return ;
+    }
+    public int getArea(){
+        //...
+        //return ;
+    }
+}
---
+++ src/D/Persona.java
@@ -0,0 +1,17 @@
+package D;
+
+public class Persona {
+    protected String numeroDeTelefono;
+    public Persona(String numeroDeTelefono){
+        super();
+        this.numeroDeTelefono = numeroDeTelefono;
+    }
+
+    public String getNumeroDeTelefono() {
+        return numeroDeTelefono;
+    }
+
+    public void setNumeroDeTelefono(String numeroDeTelefono) {
+        this.numeroDeTelefono = numeroDeTelefono;
+    }
+}
